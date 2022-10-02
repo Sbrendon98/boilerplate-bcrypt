63082036540d877d5a4b134326693c26903faf0d
Brendon Sylvestre <sylvestre.ba98@gmail.com>
2022-10-02 00:46:22 -0400

    Mounted bcrpyt

--- server.js
+++ server.js
@@ -7,7 +7,9 @@ fccTesting(app);
 const saltRounds = 12;
 const myPlaintextPassword = 'sUperpassw0rd!';
 const someOtherPlaintextPassword = 'pass123';
+const bcrypt = require("bcrypt")
 
+app.use(bcrypt)
 
 //START_ASYNC -do not remove notes, place code between correct pair of notes.
 
